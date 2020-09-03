82c4bf609fed6cb04addc76193dcb6562c1cc866
Jakub Adamik <adamik.jakub72@gmail.com>
2020-09-03 14:35:31 +0200

    Winway feature

--- board.py
+++ board.py
@@ -1,4 +1,7 @@
+import copy
+
 class Board:
+    val_strings = ["win: ", "lose: ", "tie: ", "game: ", "ratio: ", "winner: ", "winway: ", "level: ", "debug: "]
     def __init__(self):
         self.ratio = 0.
         self.win = 0
@@ -7,36 +10,25 @@ class Board:
         self.game = 0
         self.winner = 0
         self.mat = None
+        self.winway = 0
+        self.level = 0
+        self.debug = 0
         self.reset()
 
     #reset board to 0
     def reset(self):
-        if self.mat == None:
-            self.mat = [[0]*3 for i in range(3)]
-        else:
-            for row in range(3):
-                for col in range(3):
-                    self.mat[row][col] = 0
+        self.mat = [[0]*3 for i in range(3)]
 
     #put stone on board
     def put(self, stone, row, col):
         if self.mat[row][col] != 0:
             return False
-        if stone != 1 and stone != 2:
-            return False
-        if row < 0 or 3 <= row:
-            return False
-        if col < 0 or 3 <= col:
-            return False
-
         self.mat[row][col] = stone
         return True
 
     #copy gamestate from other board
     def copy(self, other):
-        for row in range(3):
-            for col in range(3):
-                self.mat[row][col] = other.mat[row][col]
+        self.mat = copy.deepcopy(other.mat)
 
     #return winner of board. If no winer return 0
     def getWinner(self):
@@ -50,12 +42,14 @@ class Board:
 
     def print(self):
         mat_iter = iter(self.mat)
-        win_str = '{:>8}'.format('win: ') + '{:<10}'.format(self.win)
-        lose_str = '{:>8}'.format('lose: ') + '{:<10}'.format(self.lose)
-        tie_str = '{:>8}'.format('tie: ') + '{:<10}'.format(self.tie)
-        game_str = '{:>8}'.format('game: ') + '{:<10}'.format(self.game)
-        ratio_str = '{:>8}'.format('ratio: ') + '{:<10}'.format(self.ratio)
-        winner_str = '{:>8}'.format('winner: ') + '{:<10}'.format(self.winner)
-        print((next(mat_iter)), win_str, game_str, sep=' | ')
-        print((next(mat_iter)), lose_str, ratio_str, sep=' | ')
-        print((next(mat_iter)), tie_str, winner_str, sep=' | ')
+        values = [self.win, self.lose, self.tie, self.game, self.ratio, self.winner, self.winway, self.level, self.debug]
+        strings = []
+        for value,string in zip(values,self.val_strings):
+            strings.append('{:>10}{:<10}'.format(string,round(value,5)))
+        strings = iter(strings)
+
+        for _ in range(3):
+            print(next(mat_iter),end = " ")
+            for _ in range(3):
+                print(next(strings, "                    "), " | ", end="")
+            print()
--- main.py
+++ main.py
@@ -2,21 +2,26 @@ from board import *
 from tree import *
 import utils
 
+
 def build_tree(root, level):
+    root.data.level = level
     winner = root.data.getWinner()
     if winner != 0:
         root.data.winner = winner
         if winner == 1:
+            root.data.winway = 1
             root.data.win += 1
         else:
             root.data.lose += 1
 
         root.data.game += 1
+        root.data.ratio = root.data.win / root.data.game
         return
 
     elif level == 9:
         root.data.tie += 1
         root.data.game += 1
+        root.data.ratio = root.data.win / root.data.game
         return
 
     player = level % 2 + 1
@@ -29,12 +34,22 @@ def build_tree(root, level):
                 son.data.put(player, row, col)
                 build_tree(son, level + 1)
 
+
+    oddLevel = level%2
+    root.data.winway = oddLevel
     for son in root.sons:
+        if oddLevel != son.data.winway:
+            root.data.winway = son.data.winway
+
         root.data.win += son.data.win
         root.data.lose += son.data.lose
         root.data.tie += son.data.tie
         root.data.game += son.data.game
+        if son.data.winway == 1:
+            root.data.debug += 1
+    root.data.debug /= len(root.sons)
     root.data.ratio = root.data.win / root.data.game
+    root.data.level = level
 
 option_str = ''
 while(option_str != 'q'):
